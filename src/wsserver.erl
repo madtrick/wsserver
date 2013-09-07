e39f6601ba7eb85cb3082262c3e06030b194ffc7
Farruco Sanjurjo <madtrick@gmail.com>
2013-09-07 11:56:52 +0200

    Configure server port

--- src/wsserver.erl
+++ src/wsserver.erl
@@ -1,7 +1,7 @@
 -module(wsserver).
 -behaviour(gen_server).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1, handle_info/2]).
 
 -record(state, {
@@ -9,16 +9,16 @@
     acceptor
   }).
 
-start_link() ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link(Options) ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, Options, []).
 
 %==========
 % Behaviour API
 %==========
-init(_Args) ->
+init(Options) ->
   process_flag(trap_exit, true),
 
-  {ok, ListenSock} = gen_tcp:listen(8082, [binary, {active, false}, {reuseaddr, true}, {buffer, 1000}]),
+  {ok, ListenSock} = gen_tcp:listen(proplists:get_value(port, Options, 8080), [binary, {active, false}, {reuseaddr, true}, {buffer, 1000}]),
   Acceptor = acceptor:start_link(?MODULE, ListenSock),
   {ok, #state{ listen_socket = ListenSock, acceptor = Acceptor}}.
 
